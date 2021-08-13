63406213e156ff90701c0f16477bb52049a93e12
Ojasvi Harsola <ojasvi@blotout.io>
2021-08-13 14:14:57 +0530

    add date partition type

--- dbt/include/athena/macros/materializations/incremental.sql
+++ dbt/include/athena/macros/materializations/incremental.sql
@@ -38,6 +38,8 @@
         {%- set value = col|string -%}
       {%- elif column_type == 'string' -%}
         {%- set value = "'" + col + "'" -%}
+      {%- elif column_type == 'date' -%}
+        {%- set value = "'" + col|string + "'" -%}
       {%- else -%}
         {%- do exceptions.raise_compiler_error('Need to add support for column type ' + column_type) -%}
       {%- endif -%}
