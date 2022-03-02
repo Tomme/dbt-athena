e8054c97477ac8b19c77e23fa4929ce86248a4b6
Darius Nicholson <48355301+nicholsondarius@users.noreply.github.com>
2022-03-02 11:42:47 +0000

    Update relation.sql

--- dbt/include/athena/macros/adapters/relation.sql
+++ dbt/include/athena/macros/adapters/relation.sql
@@ -1,7 +1,5 @@
 {% macro athena__drop_relation(relation) -%}
-  {% if config.get('incremental_strategy') != 'append' %}
-    {%- do adapter.clean_up_table(relation.schema, relation.table) -%}
-  {% endif %}
+  {%- do adapter.clean_up_table(relation.schema, relation.table) -%}
   {% call statement('drop_relation', auto_begin=False) -%}
     drop {{ relation.type }} if exists {{ relation }}
   {%- endcall %}
