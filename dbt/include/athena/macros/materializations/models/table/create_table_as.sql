80dc15de4bb36e97a8c26ca30ea99a0ac5764a68
Siarhei Nekhviadovich <Siarhei_Nekhviadovich@epam.com>
2022-04-27 16:58:59 +0200

    Athena requires write_compression for parquet

--- dbt/include/athena/macros/materializations/models/table/create_table_as.sql
+++ dbt/include/athena/macros/materializations/models/table/create_table_as.sql
@@ -4,6 +4,7 @@
   {%- set bucketed_by = config.get('bucketed_by', default=none) -%}
   {%- set bucket_count = config.get('bucket_count', default=none) -%}
   {%- set field_delimiter = config.get('field_delimiter', default=none) -%}
+  {%- set write_compression = config.get('write_compression', default='GZIP') -%}
   {%- set format = config.get('format', default='parquet') -%}
 
   create table
