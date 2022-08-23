b4dadb7c2dff0e1e7da48587dadc94f90cd40826
Brandon Telle <brandon.telle@livingsecurity.com>
2022-08-23 08:52:39 -0700

    Add unique suffix to temp table names

--- dbt/include/athena/macros/materializations/models/incremental/incremental.sql
+++ dbt/include/athena/macros/materializations/models/incremental/incremental.sql
@@ -14,7 +14,8 @@
   {% set partitioned_by = config.get('partitioned_by', default=none) %}
   {% set target_relation = this.incorporate(type='table') %}
   {% set existing_relation = load_relation(this) %}
-  {% set tmp_relation = make_temp_relation(this) %}
+  {% set tmp_suffix = athena__unique_suffix() %}
+  {% set tmp_relation = make_temp_relation(this, tmp_suffix) %}
 
   {{ run_hooks(pre_hooks, inside_transaction=False) }}
 
@@ -28,7 +29,7 @@
       {% do adapter.drop_relation(existing_relation) %}
       {% set build_sql = create_table_as(False, target_relation, sql) %}
   {% elif partitioned_by is not none and strategy == 'insert_overwrite' %}
-      {% set tmp_relation = make_temp_relation(target_relation) %}
+      {% set tmp_relation = make_temp_relation(target_relation, tmp_suffix) %}
       {% if tmp_relation is not none %}
           {% do adapter.drop_relation(tmp_relation) %}
       {% endif %}
@@ -37,7 +38,7 @@
       {% set build_sql = incremental_insert(tmp_relation, target_relation) %}
       {% do to_drop.append(tmp_relation) %}
   {% else %}
-      {% set tmp_relation = make_temp_relation(target_relation) %}
+      {% set tmp_relation = make_temp_relation(target_relation, tmp_suffix) %}
       {% if tmp_relation is not none %}
           {% do adapter.drop_relation(tmp_relation) %}
       {% endif %}
