fa6f0e594001205e58e6ac6797eae7cacc1409f1
Pavel Roslovets <p.v.roslovets@gmail.com>
2022-10-26 01:47:02 +0200

    [BUGFIX] Resolve partition deleting bug
    in insert_overwrite mode

--- dbt/adapters/athena/impl.py
+++ dbt/adapters/athena/impl.py
@@ -63,21 +63,29 @@ class AthenaAdapter(SQLAdapter):
         with boto3_client_lock:
             glue_client = boto3.client('glue', region_name=client.region_name)
         s3_resource = boto3.resource('s3', region_name=client.region_name)
-        partitions = glue_client.get_partitions(
-            # CatalogId='123456789012', # Need to make this configurable if it is different from default AWS Account ID
-            DatabaseName=database_name,
-            TableName=table_name,
-            Expression=where_condition
-        )
-        p = re.compile('s3://([^/]*)/(.*)')
-        for partition in partitions["Partitions"]:
-            logger.debug("Deleting objects for partition '{}' at '{}'", partition["Values"], partition["StorageDescriptor"]["Location"])
-            m = p.match(partition["StorageDescriptor"]["Location"])
-            if m is not None:
-                bucket_name = m.group(1)
-                prefix = m.group(2)
-                s3_bucket = s3_resource.Bucket(bucket_name)
-                s3_bucket.objects.filter(Prefix=prefix).delete()
+        continue_deleting: bool = True
+        next_token: str = ''
+        while continue_deleting:
+            partitions = glue_client.get_partitions(
+                # CatalogId='123456789012', # Need to make this configurable if it is different from default AWS Account ID
+                DatabaseName=database_name,
+                TableName=table_name,
+                Expression=where_condition,
+                NextToken=next_token
+            )
+            p = re.compile('s3://([^/]*)/(.*)')
+            for partition in partitions["Partitions"]:
+                logger.debug("Deleting objects for partition '{}' at '{}'", partition["Values"], partition["StorageDescriptor"]["Location"])
+                m = p.match(partition["StorageDescriptor"]["Location"])
+                if m is not None:
+                    bucket_name = m.group(1)
+                    prefix = m.group(2)
+                    s3_bucket = s3_resource.Bucket(bucket_name)
+                    s3_bucket.objects.filter(Prefix=prefix).delete()
+            if 'NextToken' in partitions:
+                next_token = partitions['NextToken']
+            else:
+                continue_deleting = False
 
     @available
     def clean_up_table(
